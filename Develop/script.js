632980d4bb4264a6be53c230182858c022f1ded1
jalland <jjalland@gmail.com>
2023-10-27 12:28:23 -0400

    Initial protptype of application complete

--- Develop/script.js
+++ Develop/script.js
@@ -8,16 +8,50 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $(".saveBtn").on("click",function(){
+    text = $(this).parent().children('textarea').val()
+    id = $(this).parent().attr('id');
+    localStorage.setItem(id,text)
+  })
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  hours = [9,10,11,12,13,14,15,16,17]
+  for(i=0;i<hours.length;i++){
+      if(dayjs().format("H")<hours[i]){
+        $("#hour-"+(i+9)).removeClass("past")
+        $("#hour-"+(i+9)).removeClass("present")
+        $("#hour-"+(i+9)).addClass("future")
+      }
+      else if(dayjs().format("H")==hours[i]){
+        $("#hour-"+(i+9)).removeClass("past")
+        $("#hour-"+(i+9)).addClass("present")
+        $("#hour-"+(i+9)).removeClass("future")
+      }
+      else if (dayjs().format("H")>hours[i]){
+        $("#hour-"+(i+9)).addClass("past")
+        $("#hour-"+(i+9)).removeClass("present")
+        $("#hour-"+(i+9)).removeClass("future")
+      }
+  }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  hours = [9,10,11,12,13,14,15,16,17]
+  for(i=0;i<hours.length;i++){
+    currentText = localStorage.getItem("hour-"+(i+9))
+    if(currentText !== null ){
+      $("#hour-"+(i+9)).children('textarea').text(currentText)
+    }
+  }
+
   //
   // TODO: Add code to display the current date in the header of the page.
+  $("#currentDay").text(dayjs().format("dddd MMMM M, YYYY"))
 });
